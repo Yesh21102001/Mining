6955f9d548c0978790d96d556e58830635598e01
Yeswanth Kumar <yeswanthkona021@gmail.com>
2026-04-22 11:34:12 +0530

    first commit

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export const dynamic = "force-static";
+
+export default function sitemap() {
+    return [
+        {
+            url: "https://huaintrade.com",
+            lastModified: "2026-03-16",
+        },
+    ];
+}
