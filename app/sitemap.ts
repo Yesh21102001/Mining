ec89470f57bdccdab5ad4fb258e7ab780913256b
Kona Yeswanth Kumar <yeswanthkona021@gmail.com>
2026-06-17 17:19:08 +0530

    first commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,26 @@
 export const dynamic = "force-static";
 
 export default function sitemap() {
-    return [
-        {
-            url: "https://huaintrade.com",
-            lastModified: "2026-03-16",
-        },
-    ];
+  return [
+    {
+      url: "https://www.huaintrade.com/",
+      lastModified: "2026-06-17",
+    },
+    {
+      url: "https://www.huaintrade.com/about/",
+      lastModified: "2026-06-17",
+    },
+    {
+      url: "https://www.huaintrade.com/services/",
+      lastModified: "2026-06-17",
+    },
+    {
+      url: "https://www.huaintrade.com/staff/",
+      lastModified: "2026-06-17",
+    },
+    {
+      url: "https://www.huaintrade.com/contact/",
+      lastModified: "2026-06-17",
+    },
+  ];
 }
